a462d6f5e72042620d2da752a33aeea9a1787223
Valdo <85319750+valdodotcom@users.noreply.github.com>
2023-02-26 00:56:24 +0000

    Typed CSS Animation on Home Page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Typed from 'typed.js';
 
 @Component({
   selector: 'app-home',
@@ -12,4 +13,18 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  ngAfterViewInit() {
+    this.initTyped();
+  }
+  
+  private initTyped() {
+    const typed = new Typed('.intro-text', {
+      strings: ["Computer Engineer", "Front End Developer", "Full Stack Developer"],
+      startDelay: 1000,
+      backDelay: 1000,
+      typeSpeed: 40,
+      backSpeed: 80,
+      loop: true,
+    });
+  }
 }
